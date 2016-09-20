63006308915b1a3c8305618d636fdc8d266f6c40
Awk Jiang <shengjhih@gmail.com>
2016-09-20 21:54:45 +0800

    revert setup.py

--- setup.py
+++ setup.py
@@ -1,36 +1,25 @@
 #!/usr/bin/env python
-
-import ez_setup
-ez_setup.use_setuptools(version="24")
-
-from setuptools import setup, find_packages
-import platform
-
-def read(filename):
-    with open(filename) as fp:
-        return fp.read()
+try:
+  import os
+  from setuptools import setup, find_packages
+except ImportError:
+  from distutils.core import setup
 
 setup(
-    name='pywifi',
-    version='0.9.6',
-    author='Jiang Shengh-Jhih',
-    author_email='shengjhih@gmail.com',
-    description="A cross-platform module for manipulating WiFi devices.",
-    long_description=read("README.rst"),
-    packages=find_packages(),
-    url='https://github.com/awkman/pywifi', 
-    license='MIT',
-    install_requires=[
-        'comtypes>=1.1'
-    ],
-    classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Internet',
-        'Topic :: Utilities'
-    ],
-    keywords=['wifi', 'wireless', 'Linux', 'Windows'], 
+  name = 'pywifi',
+  version = '0.9.7',
+  author = 'Jiang Shengh-Jhih',
+  author_email = 'shengjhih@gmail.com',
+  description = "A cross-platform module for manipulating WiFi devices.",
+  packages=find_packages(),
+  url = 'https://github.com/awkman/pywifi', 
+  license = 'MIT',
+  download_url = 'https://github.com/awkman/pywifi/archive/master.zip', 
+  classifiers = [
+      'Intended Audience :: Developers',
+      'Topic :: Utilities',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3.5',
+  ],
+  keywords = ['wifi', 'wireless', 'Linux', 'Windows'], 
 )
